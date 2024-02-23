2868edbd685bab8ea192133cd7121186b96d759e
Mark Baca <markbaca42@gmail.com>
2024-02-23 15:51:43 -0600

    removed excess files

--- code.py
+++
@@ -1,7 +0,0 @@
-import spotipy  
-from spotipy.oauth2 import SpotifyOAuth  
-  
-sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPE)  
-  
-access_token = sp_oauth.get_access_token()  
-refresh_token = sp_oauth.get_refresh_token()  
